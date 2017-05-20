b7b141b58d8446785ce8bac1209b6b5d32ab06db
flavio <fds%322911>
2017-05-20 10:52:29 -0300

    criando component

--- host-map-web/src/app/app.module.ts
+++ host-map-web/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FdSearchBarComponent } from './fd-search-bar/fd-search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FdSearchBarComponent
   ],
   imports: [
     BrowserModule,
