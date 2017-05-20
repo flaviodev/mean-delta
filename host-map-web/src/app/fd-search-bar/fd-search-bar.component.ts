22853183e0a3ffe52cf3f485b7f7417f57fb0521
flavio <fds%322911>
2017-05-20 10:53:12 -0300

    criando component

---
+++ host-map-web/src/app/fd-search-bar/fd-search-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fd-fd-search-bar',
+  templateUrl: './fd-search-bar.component.html',
+  styleUrls: ['./fd-search-bar.component.css']
+})
+export class FdSearchBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
