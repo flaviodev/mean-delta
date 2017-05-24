e75b0410e27b36eb1f2e2e230867cee1cf3b8933
flavio <fds%322911>
2017-05-23 21:16:32 -0300

    .

--- host-map-web/src/app/historico-page/historico-page.component.ts
+++ host-map-web/src/app/historico-page/historico-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Localizacao} from "../model/Localizacao.model";
 
 @Component({
   selector: 'fd-historico-page',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./historico-page.component.css']
 })
 export class HistoricoPageComponent implements OnInit {
+
+  historicos: Array<Localizacao> = [ new Localizacao(
+    33.749,-121.8914, "FaceBook", "12.23.22.33", "United States", "California", "CA", "San Francisco", "facebook.com")];
 
   constructor() { }
 
--- host-map-web/src/app/search-bar/search-bar.component.ts
+++ host-map-web/src/app/search-bar/search-bar.component.ts
@@ -28,7 +28,33 @@ export class FdSearchBarComponent implements OnInit {
       .get('http://ip-api.com/json/' + this.dominio)
       .subscribe(
         (data) => {
+          this.gravarHistorico(data);
           this.emiterLocalizacao.emit(data);
+        },
+        (error) => console.error(error)
+      );
+  }
+
+  gravarHistorico(_data) {
+
+    let localizacao: Localizacao = new Localizacao();
+    localizacao.id = "";
+    localizacao.dominio = this.dominio;
+    localizacao.isp = _data.isp;
+    localizacao.query = _data.query;
+    localizacao.lat = _data.lat;
+    localizacao.lon = _data.lon;
+    localizacao.country = _data.country;
+    localizacao.regionName = _data.regionName;
+    localizacao.region = _data.region;
+    localizacao.city = _data.city;
+    localizacao.date = new Date();
+
+    this.httpClient
+      .post('http://localhost:8080/salvar', localizacao)
+      .subscribe(
+        (data) => {
+          console.log(data);
         },
         (error) => console.error(error)
       );
