cb62d0a1b1cb29394855925de1316ba6d44da348
flavio <fds%322911>
2017-05-27 11:31:26 -0300

    criando end points

--- host-map-api/routes/localizacao.route.js
+++ host-map-api/routes/localizacao.route.js
@@ -1,8 +1,29 @@
 const express = require('express');
 let router = express.Router();
 
-router.get('/save', (req,res, next) => {
-   res.send("save");
+router.get('/', (req,res, next) => {
+
+   res.status(200).json({nome : 'Flávio'});
 });
+
+router.post('/', (req,res, next) => {
+
+    let data = req.body;
+    data.name = 'teste';
+    res.status(201).json(data);
+});
+
+router.put('/', (req,res, next) => {
+
+    let data = req.body;
+    res.status(200).json(data);
+});
+
+router.delete('/', (req,res, next) => {
+
+    res.status(200).json({nome : 'Flávio'});
+});
+
+
 
 module.exports = router;
